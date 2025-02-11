d1aa014b32b51318aad90a31ccf1f4cdd16e0e57
dedencreatifdev <dedencreatifdeveloper@gmail.com>
2025-02-11 16:30:00 +0700

    .

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>AdminLTE 3 | {{ $title ?? 'Page Title' }}</title>
+    <title>{{ config('app.name') }} | {{ $title ?? 'Page Title' }}</title>
 
     <!-- Google Font: Source Sans Pro -->
     <link rel="stylesheet"
